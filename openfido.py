efe65642bd8ab40e81324612e454c86cf49d3736
David P. Chassin <dchassin@slac.stanford.edu>
2022-02-09 12:11:14 -0800

    Update openfido.py

--- openfido.py
+++ openfido.py
@@ -1,52 +1,38 @@
 """Address resolution pipeline
 
-The address resolution pipeline resolves addresses and locations.
+Census TIGER geographic data retrieval.
 
 CONFIG
 ------
 
 The following parameters are recognized in `config.csv`:
 
-* DATA: The CSV data file name contain address or location (required)
+* `INPUT_FILENAME`: Specifies the input filename. Optional, default is `input.csv`.
 
-* REVERSE: Boolean value indicating whether the data contains locations
-  (False, default) or addresses (True)
+* `OUTPUT_FILENAME`: Specifies the output filename. Optional, default is `output.csv`.
 
-* PROVIDER: Resolver provider (default is "nominatim")
+* `STATE_FIELDS`: Specifies the state fields to retrieve. Valid fields are `REGION`, `DIVISION`, `STATEFP`, `STATENS`, `GEOID`, `STUSPS`, `NAME`, `LSAD`, `MTFCC`, `FUNCSTAT`, `ALAND`, `AWATER`, `INTPTLAT`, `INTPTLON`, `geometry`. Optional, default is `*`.  
 
-* USER_AGENT: Resolver user agent (default is "csv_user_ht")
-
-* TIMEOUT: Resolver timeout in seconds (default is 5)
-
-* RETRIES: Resolver retry limit (default is 5)
-
-* SLEEP: Resolver sleep between retries in seconds (default 1)
+* `ZIPCODE_FIELDS`: Specifies the zipcode fields to retrieve. `ZCTA5CE10`, `GEOID10`, `CLASSFP10`, `MTFCC10`, `FUNCSTAT10`, `ALAND10`, `AWATER10`, `INTPTLAT10`, `INTPTLON10`, `geometry`. Optional, default is `*`.
 
 INPUT
 -----
 
-The format of the input depends on the value of REVERSE in config.csv.
+The following files are accepted as input folder.
 
-* REVERSE=False
-
-The CSV file must include a *latitude* and *longitude* column.
-
-* REVERSE=True
-
-The CSV file must include an *address* column.
+* `input.csv`: The input file must provide the `latitude` and `longitude` fields.
 
 OUTPUT
 ------
 
-The format of the output depends on the value of REVERSE in config.csv.
+The following files are generated in the output folder.
 
-* REVERSE=False
+* `output.csv`: The output file will include the requested state and zipcode fields, as well as all the input fields.
 
-The CSV file will include an *address* column.
+SEE ALSO
+--------
 
-* REVERSE=True
-
-The CSV file will include a *latitude* and *longitude* column.
+* https://www.census.gov/programs-surveys/geography/guidance/tiger-data-products-guide.html
 
 """
 
